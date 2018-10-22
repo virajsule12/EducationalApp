a466677583ccc5b6c1035321dc1c1aaf918e2594
virajsule12 <35614574+virajsule12@users.noreply.github.com>
2018-10-22 14:30:50 -0500

    anotha one

--- app/src/main/java/com/example/rc211/educationapp/MainActivity.java
+++ app/src/main/java/com/example/rc211/educationapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.rc211.educationapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        handleEasy();
+    }
+
+    public void handleEasy(){
+        Button easy = (Button) findViewById(R.id.easybtn);
+        easy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,EasyGameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
